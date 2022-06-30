69503869e5c81f12e4775beb3bb49db54f5d5af2
akeem.safiriyu <aosafiriyu@gmail.com>
2022-06-30 09:52:50 +0900

    modification to ridge regression

--- dataTraining.m
+++ dataTraining.m
@@ -4,15 +4,17 @@ clear;
 close all;
 
 %% code implementation
-filename = 'NARMA10_task_GVD_NL_midpoint2.csv';
-data = readmatrix(filename);
-X_train = data(1:350,1:4096);
-Y_train = data(1:350,4098);
-X_test = data(351:500,1:4096);
-Y_test = data(351:500,4098);
+inputfilename = 'NARMA_TASKIN_RS.csv';
+outputfilename = 'NARMA_TASKOUT_RS.csv';
+dataIN = readmatrix(inputfilename);
+dataOUT = readmatrix(outputfilename);
+X_train = dataIN(1:900,:);
+Y_train = dataOUT(1:900);
+X_test = dataIN(901:1000,:);
+Y_test = dataOUT(901:1000);
 
 %% Ridge regression Algorithm
-ridge_coefficient = 0.005; %0: 1e-5: 5e-3;
+ridge_coefficient = 0.00005; %0: 1e-5: 5e-3;
 scaling_factor = 0;
 model = ridge(Y_train,X_train,ridge_coefficient,scaling_factor);
 
@@ -23,21 +25,21 @@ else
 end
     %Compute cost function
 MSE = sum((Y_test - Y_predict).^2)/length(Y_predict);
-variance_y = sum((Y_predict - mean(Y_predict)).^2)/length(Y_predict);
+variance_y = var(Y_predict);
 NMSE_ridge = MSE / variance_y;
 figure; plot((1:length(Y_predict)),Y_predict,'*'); hold on; plot((1:length(Y_predict)),Y_test,'o');
 
 %% Lasso Regression Algorithm
-alpha = 0.75;
-fold_validation = 20;
-[model,FitInfo] = lasso(X_train,Y_train,'Alpha',alpha,'CV',fold_validation);
-idxLambda1SE = FitInfo.Index1SE;
-Weight = model(:,idxLambda1SE);
-constant_coef = FitInfo.Intercept(idxLambda1SE);
-Y_predict = X_test * Weight + constant_coef;
-%Compute cost function
-MSE = sum((Y_test - Y_predict).^2)/length(Y_predict);
-variance_y = sum((Y_predict - mean(Y_predict)).^2)/length(Y_predict);
-NMSE_lasso = MSE / variance_y;
-figure;plot((1:length(Y_predict)),Y_predict,'*'); hold on; plot((1:length(Y_predict)),Y_test,'o');
+% alpha = 0.1;
+% fold_validation = 20;
+% [model,FitInfo] = lasso(X_train,Y_train,'Alpha',alpha,'CV',fold_validation);
+% idxLambda1SE = FitInfo.Index1SE;
+% Weight = model(:,idxLambda1SE);
+% constant_coef = FitInfo.Intercept(idxLambda1SE);
+% Y_predict = X_test * Weight + constant_coef;
+% Compute cost function
+% MSE = sum((Y_test - Y_predict).^2)/length(Y_predict);
+% variance_y = sum((Y_predict - mean(Y_predict)).^2)/length(Y_predict);
+% NMSE_lasso = MSE / variance_y;
+% figure;plot((1:length(Y_predict)),Y_predict,'*'); hold on; plot((1:length(Y_predict)),Y_test,'o');
 
