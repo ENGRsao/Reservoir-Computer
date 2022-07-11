36ae1cae2d6b959f49aba72241feeff48d9cdbc5
akeem.safiriyu <aosafiriyu@gmail.com>
2022-07-11 22:11:46 +0900

    update simulation parameters

--- loadSimulationParameters.m
+++ loadSimulationParameters.m
@@ -4,7 +4,7 @@ function [Pulse,Fiber] = loadSimulationParameters()
     
     %% Pulse Parameters
     Pulse.fft_point = 4096;   %FFT points   N = 2^10
-    Pulse.time_span = 32;     %Time period of the desire signal
+    Pulse.time_span = 64;     %Time period of the desire signal
     Pulse.delta_tau = (2*Pulse.time_span)/Pulse.fft_point;  % smallest division of time
     Pulse.tau = (-Pulse.fft_point/2:Pulse.fft_point/2-1)*Pulse.delta_tau; %time duration spread out of the signal
     Pulse.omega = [(0:Pulse.fft_point/2-1) (-Pulse.fft_point/2:-1)] .*(pi/Pulse.time_span);
@@ -15,25 +15,25 @@ function [Pulse,Fiber] = loadSimulationParameters()
     Pulse.guassian_order = 3;
     Pulse.chirp = 0;
     Pulse.speed_of_light = 3 * 10^5; %in nm/ps
-    Pulse.wavelength = 1500; % in nm
-    Pulse.Peak_power = 0.90; %in Watt
+    Pulse.wavelength = 1550; % in nm
+    Pulse.Peak_power = 0.50; %in Watt
     
     %% Fiber Parameters
         %Dispersion parameters
     Fiber.D = 17; %GVD parameter in ps/nm km
     Fiber.Beta2 = 1 * (Pulse.wavelength^2 * Fiber.D)/(2*pi*Pulse.speed_of_light); % ps^2/Km ; Dispersion Constant
     Fiber.dispersion_length = Pulse.T_nat^2 / abs(Fiber.Beta2); %Km
-    Fiber.length = 1.2 * Fiber.dispersion_length;  %Km
+    Fiber.length = 1.5 * Fiber.dispersion_length;  %Km
     
         %NonLinear Parameters 
-    Fiber.gamma = 1.3; %W^-1.km^-1 for standard single mode fiber; Nolinear Coefficeint, 1.76
+    Fiber.gamma = 1.76; %W^-1.km^-1 for standard single mode fiber; Nolinear Coefficeint, 1.76
     Fiber.alpha = 0.2;  % fiber loss db/km
     Fiber.L_effective = (1 - exp(-1 * Fiber.alpha * Fiber.length))/Fiber.alpha; %Km effective length
     Fiber.NonLinear_length = 1 / (Fiber.gamma * Pulse.Peak_power);   %Km
     
         %Numerical Simulation parameter
     Pulse.soliton_order  = round(sqrt(Fiber.dispersion_length/Fiber.NonLinear_length));  %constant
-    Fiber.Division_factor = 20;
+    Fiber.Division_factor = 200;
         
         %Input signal type
     Pulse.sech = sech(Pulse.Normalize_tau) .* exp(-1i*0.5*Pulse.chirp.*Pulse.Normalize_tau.^2); %sech signal
