8696ccfccfa41e5c175084c53519983396aa4f72
akeem.safiriyu <aosafiriyu@gmail.com>
2022-07-11 22:09:45 +0900

    update code

--- NonLinearEffect.m
+++ NonLinearEffect.m
@@ -1,5 +1,5 @@
 function [NonLinearResponse_t,NonLinearResponse_f] = NonLinearEffect(signal,solitonOrder,FiberLength)
     Phase_factor = (1i * solitonOrder^2 * FiberLength)  ;  %phase factor
     NonLinearResponse_t =  signal .* exp( Phase_factor.* abs(signal).^2); % Non linear effect over the span of the fiber on the original signal
-    NonLinearResponse_f = fftshift(fft(NonLinearResponse_t));
+    NonLinearResponse_f = (fft(NonLinearResponse_t));
 end
