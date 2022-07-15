2e0b809d0c0d7edc8bbf11030ca7682dcadebfad
akeem.safiriyu <aosafiriyu@gmail.com>
2022-07-15 14:05:17 +0900

    correction of dataset importation

--- loadDataset.m
+++ loadDataset.m
@@ -2,8 +2,8 @@ function [input,output] = loadDataset()
     %% Load Dataset base on the required Task
     % val - Input valu determining the type of dataset
     %   val = 1  - NARMA task
-    filename = 'NARMA10_output.csv';
-    output = readmatrix(filename);
-    input_filename = 'NARMA10_input.csv';
+    input_filename = 'NARMA10_input.csv';    
     input = readmatrix(input_filename);
+    output_filename = 'NARMA10_output.csv';
+    output = readmatrix(output_filename);
 end
