d991a09f6da299c78e6fb364c12e2807a2a036c1
akeem.safiriyu <aosafiriyu@gmail.com>
2022-07-20 11:51:54 +0900

    update code using multiple input delay algorithm

--- ReservoirComputer_NL_GVD_MaskedBeforeMZM.m
+++ ReservoirComputer_NL_GVD_MaskedBeforeMZM.m
@@ -13,8 +13,8 @@ plot(Pulse.Normalize_tau,abs(SSFM_t).^2);
 %% Reservoir Network application to NARMA10 task 
 %input_mask = [0.7 0.2 0.4 0.5 0.9 0.8 0.1 0.4 0.3 0.6 0.95 0.3 0.99	0.35 0.2 0.7 0.8 0.6 0.1 0.4 0.2 0.2 0.1 0.9 0.8 0.4 0.8 0.75 0.6	0.4	0.35 0.15 0.3 0.55 0.45	0.65 0.5	0.8	0.1	0.4	0.6	0.2	0.6	0.1	0.8	0.7	0.3	0.4	1.0	0.3];
 input_mask = [0.2 0.5 0.9 0.4 0.8 0.4 0.0 0.2 0.9 0.7 0.5 0.2 0.6 1.0 0.7 0.4 0.4 0.8 0.5 0.2 0.8 0.5 0.3 0.6 0.0 0.7 0.0 0.5 0.3 0.4 0.7 0.8 0.2 0.7 0.4 0.3 0.2 0.6 0.7 0.1 0.4 0.6 0.9 0.8 0.7 0.3 0.9 0.1 0.2 0.4];
-u_n = Input(1:10010);
-Y_n = Output(1:10010);
+u_n = Input(1:10500);
+Y_n = Output(1:10500);
 
 mask_len = length(input_mask);
 signal_len = length(u_n);
@@ -25,7 +25,7 @@ readout_f = zeros(signal_len,Pulse.fft_point);
 readout_RS = zeros(signal_len, mask_len);
 %% Algorithm test, Single delay loop with T = (N+1)Theta
 % for i = 1:signal_len
-%     time_multiplexed_signal = input_mask .* u_n(i) + 0.10;   % time multiplexing of input signal 
+%     time_multiplexed_signal = input_mask .* u_n(i);   % time multiplexing of input signal    
 %     for k = 1:length(time_multiplexed_signal)-1   %T = (N+1)Theta
 %         modulated_signal = time_multiplexed_signal(k) .* Pulse.guassian; %convert data to guassian input
 %         MZM_output = sqrt(Pulse.Peak_power) * sin((pi/(2*Fiber.MZM_Vpi)) .* modulated_signal); %modulation of input data
@@ -51,8 +51,8 @@ readout_RS = zeros(signal_len, mask_len);
 % end  
 
 %% Multiple Delay algorithm according to paper
-input_mask2 = [0.910000000000000	0.130000000000000	0.910000000000000	0.630000000000000	0.100000000000000	0.280000000000000	0.550000000000000	0.960000000000000	0.960000000000000	0.160000000000000	0.970000000000000	0.960000000000000	0.490000000000000	0.800000000000000	0.140000000000000	0.420000000000000	0.920000000000000	0.790000000000000	0.960000000000000	0.660000000000000	0.0400000000000000	0.850000000000000	0.930000000000000	0.680000000000000	0.760000000000000	0.740000000000000	0.390000000000000	0.660000000000000	0.170000000000000	0.710000000000000	0.0300000000000000	0.280000000000000	0.0500000000000000	0.100000000000000	0.820000000000000	0.690000000000000	0.320000000000000	0.950000000000000	0.0300000000000000	0.440000000000000	0.380000000000000	0.770000000000000	0.800000000000000	0.190000000000000	0.490000000000000	0.450000000000000	0.650000000000000	0.710000000000000	0.750000000000000	0.280000000000000];
-delayVal = 9;
+input_mask2 = [0.91	0.13	0.91	0.63	0.10	0.28	0.55	0.96	0.96	0.16	0.97	0.96	0.49	0.80	0.14	0.42	0.92	0.79	0.96	0.66	0.04	0.85	0.93	0.68	0.76	0.74	0.39	0.66	0.17	0.71	0.03	0.28	0.05	0.10	0.82	0.69	0.32	0.95	0.03	0.44	0.38	0.77	0.8	0.2	0.49	0.45	0.65	0.7	0.8	0.3];
+delayVal = 10;
 delayed_u_n = [zeros(delayVal,1);u_n(1:(signal_len-delayVal))];
 %Algorithm test
 for i = delayVal+1:signal_len
@@ -61,27 +61,27 @@ for i = delayVal+1:signal_len
         modulated_signal = time_multiplexed_signal(k) .* Pulse.guassian; %convert data to guassian input
         MZM_output = sqrt(Pulse.Peak_power) * sin((pi/(2*Fiber.MZM_Vpi)) .* modulated_signal); %modulation of input data
         memory_modulated_signal = (MZM_output .* sqrt(1 - Fiber.Inputcoupling)) + (1i * sqrt(Fiber.Inputcoupling) .* reservoirmemory(k,:)); %fiber coupling
-        [SSFM_t,SSFM_f]  = NonLinearEffect(memory_modulated_signal ,Pulse.soliton_order,Fiber.length);
-        %[SSFM_t,SSFM_f] = splitStepMethod(memory_modulated_signal,Pulse.soliton_order,Fiber.length, Fiber.Beta2, Fiber.Division_factor, Pulse.omega ); %split step algorithm to resolve GVD and NL effect in the fiber
+        %[SSFM_t,SSFM_f]  = NonLinearEffect(memory_modulated_signal ,Pulse.soliton_order,Fiber.length);
+        [SSFM_t,SSFM_f] = splitStepMethod(memory_modulated_signal,Pulse.soliton_order,Fiber.length, Fiber.Beta2, Fiber.Division_factor, Pulse.omega ); %split step algorithm to resolve GVD and NL effect in the fiber
         %SSFM_f =  SSFM_f .* (sqrt(2*pi)/(Pulse.fft_point*Pulse.delta_tau));
         reservoirmemory(k+1,:) = SSFM_t .* sqrt(Fiber.Outputcoupling);  
-        readout_RS(i,k) = mean(abs(SSFM_t(1024:3068).*sqrt(1 - Fiber.Outputcoupling)).^2);
+        readout_RS(i,k) = mean(abs(SSFM_t.*sqrt(1 - Fiber.Outputcoupling)).^2);
         %figure; plot(Pulse.Normalize_tau,abs(reservoirmemory(k,:)).^2);
     end
     %%last data
     modulated_signal = time_multiplexed_signal(length(time_multiplexed_signal)); % .* Pulse.guassian;
     MZM_output = sqrt(Pulse.Peak_power) * sin((pi/(2*Fiber.MZM_Vpi)) .* modulated_signal);
     memory_modulated_signal = (MZM_output .* sqrt(1 - Fiber.Inputcoupling)) + (1i * sqrt(Fiber.Inputcoupling) .* reservoirmemory(k,:)); %fiber coupling
-    %[SSFM_t,SSFM_f] = splitStepMethod(memory_modulated_signal,Pulse.soliton_order,Fiber.length, Fiber.Beta2, Fiber.Division_factor, Pulse.omega ); %split step algorithm to resolve GVD and NL effect in the fiber
-    [SSFM_t,SSFM_f]  = NonLinearEffect(memory_modulated_signal ,Pulse.soliton_order,Fiber.length);
+    [SSFM_t,SSFM_f] = splitStepMethod(memory_modulated_signal,Pulse.soliton_order,Fiber.length, Fiber.Beta2, Fiber.Division_factor, Pulse.omega ); %split step algorithm to resolve GVD and NL effect in the fiber
+    %[SSFM_t,SSFM_f]  = NonLinearEffect(memory_modulated_signal ,Pulse.soliton_order,Fiber.length);
     %SSFM_f =  SSFM_f .* (sqrt(2*pi)/(Pulse.fft_point*Pulse.delta_tau));
     reservoirmemory(1,:) = SSFM_t .* sqrt(Fiber.Outputcoupling);  
     readout_t(i,:) = abs(reservoirmemory(round(k/2),:)).^2;
-    readout_RS(i,mask_len) = mean(abs(SSFM_t(1024:3068).*sqrt(Fiber.Outputcoupling)).^2);
+    readout_RS(i,mask_len) = mean(abs(SSFM_t.*sqrt(Fiber.Outputcoupling)).^2);
     %figure; plot(Pulse.Normalize_tau,readout_t(i,:));
 end  
 
 %% output data to file
-writematrix(readout_t,'NARMA_TASKIN_INIT2.csv');
+%writematrix(readout_t,'NARMA_TASKIN_INIT2.csv');
 writematrix(readout_RS,'NARMA_TASKIN_RS2.csv');
 writematrix(Y_n,'NARMA_TASKOUT_RS2.csv');
